408a10f15b2cae2f2021bba939fbdce9f4f9aec9
wenyue <gzhuangjinwen@game.ntes>
2018-01-19 18:24:16 +0800

    modified:   macro.py 	modified:   tools/picker.py

--- tools/picker.py
+++ tools/picker.py
@@ -6,12 +6,13 @@ import os
 import macro
 import util
 import random
-from functools import reduce 
+from functools import reduce
 
 
 def loadImage(filename):
 	ori_img = Image.open(filename)
-	w, h = int(ori_img.width * macro.NORMALIZE_SCALE), int(ori_img.height * macro.NORMALIZE_SCALE)
+	w, h = int(ori_img.width * macro.NORMALIZE_SCALE), int(
+		ori_img.height * macro.NORMALIZE_SCALE)
 	img = ori_img.resize((w, h))
 	img.filename = filename
 	img.ori_img = ori_img
@@ -24,7 +25,8 @@ def calSimilarity(li, ri):
 	w, h = li.size
 	lp = li.load()
 	rp = ri.load()
-	samePixelNum = sum(1 if lp[i, j] == rp[i, j] else 0 for i in range(w) for j in range(h))
+	samePixelNum = sum(
+		1 if lp[i, j] == rp[i, j] else 0 for i in range(w) for j in range(h))
 	return float(samePixelNum) / (w * h)
 
 
@@ -40,7 +42,8 @@ def getDiffRect(li, ri):
 	for i in range(w):
 		for j in range(h):
 			if lp[i, j] != rp[i, j]:
-				xmax, xmin, ymax, ymin = max(xmax, i), min(xmin, i), max(ymax, j), min(ymin, j)
+				xmax, xmin, ymax, ymin = max(xmax, i), min(xmin, i), max(ymax, j), min(
+					ymin, j)
 	return xmax, xmin, ymax, ymin
 
 
@@ -58,7 +61,8 @@ def copyImagesToTmp(images):
 		for aid, action in enumerate(scene):
 			for img in action:
 				util.copy(img.filename,
-					os.path.join(macro.TMP_PATH, '%04d/%02d/%s' % (sid, aid, macro.IMAGE_NAME)))
+						  os.path.join(macro.TMP_PATH,
+									   '%04d/%02d/%s' % (sid, aid, macro.IMAGE_NAME)))
 
 
 def genFaceToTmp(images):
@@ -66,10 +70,12 @@ def genFaceToTmp(images):
 		for aid, action in enumerate(scene):
 			if len(action) == 1:
 				util.copy(action[0].filename,
-					os.path.join(macro.TMP_PATH, '%04d/%02d/%s' % (sid, aid, macro.FACE_NAME)))
+						  os.path.join(macro.TMP_PATH,
+									   '%04d/%02d/%s' % (sid, aid, macro.FACE_NAME)))
 				continue
 
 			rect = [float('inf'), float('inf'), 0, 0]
+
 			def func(li, ri):
 				xmax, xmin, ymax, ymin = getDiffRect(li, ri)
 				rect[0] = min(rect[0], xmin)
@@ -77,28 +83,32 @@ def genFaceToTmp(images):
 				rect[2] = max(rect[2], xmax)
 				rect[3] = max(rect[3], ymax)
 				return ri
+
 			reduce(func, action)
 			rect = [val / macro.NORMALIZE_SCALE for val in rect]
 			for img in action:
-				facePath = os.path.join(macro.TMP_PATH, '%04d/%02d/%s' % (sid, aid, macro.FACE_NAME))
+				facePath = os.path.join(macro.TMP_PATH,
+										'%04d/%02d/%s' % (sid, aid, macro.FACE_NAME))
 				os.makedirs(facePath, exist_ok=True)
 				basename = os.path.basename(img.filename)
 				facePath = os.path.join(facePath, basename)
 				face = img.ori_img.crop(rect)
 				face.save(facePath)
 
-				
+
 def pickImagesToTmp(images):
 	picks = []
 	for sid, scene in enumerate(images):
 		for action in scene:
 			if picks:
 				imgs = random.sample(action, min(len(action), macro.SAMPLE_IMAGE_NUM))
-				imgs = [(sum(calSimilarity(img, pick) for pick in picks), img) for img in imgs]
-				_, pick = min(imgs, key=lambda x: x[0])
+				cpicks = picks[-macro.COMPARE_IMAGE_NUM:]
+				values = [sum(calSimilarity(img, pick) for pick in cpicks) for img in imgs]
+				_, pick = min(zip(values, imgs), key=lambda x: x[0])
 			else:
 				pick = random.choice(action) if random.randint(0, 1) else action[0]
-			util.copy(pick.filename, os.path.join(macro.TMP_PATH, '%04d/%s' % (sid, macro.PICK_NAME)))
+			util.copy(pick.filename,
+					  os.path.join(macro.TMP_PATH, '%04d/%s' % (sid, macro.PICK_NAME)))
 			picks.append(pick)
 
 
